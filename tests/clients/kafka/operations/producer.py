26cc1e76e3f5563374dc2deac6751f3d4e3aadc7
UberSurale <fazon.tropol@gmail.com>
2026-09-18 16:57:47 +0700

    добавлены кафка продюсеры

---
+++ tests/clients/kafka/operations/producer.py
@@ -0,0 +1,53 @@
+import time
+
+from tests.clients.kafka.producer import KafkaProducerTestClient
+from tests.schemas.operations import OperationEventTestSchema
+from tests.tools import allure
+
+from tests.config import test_settings
+from tests.tools.logger import get_test_logger
+from tests.types.operations import OperationTestStatus, OperationTestType
+
+
+class OperationsKafkaProducerTestClient(KafkaProducerTestClient):
+
+    @allure.step("Produce operation event")
+    def produce_operation_event_api(
+        self,
+        event: OperationEventTestSchema,
+    ) -> OperationEventTestSchema:
+        self.produce(
+            topic="operations-service.operation-even.inbox",
+            value=event.model_dump_json()
+        )
+        self.flush_all()
+        time.sleep(test_settings.operations_processing_wait_timeout)
+
+
+    @allure.step("Produce in progress purchase operation event")
+    def produce_in_progress_purchase_operation_event(self) -> OperationEventTestSchema:
+        return self.produce_operation_event_api(
+            OperationEventTestSchema(
+                type=OperationTestType.PURCHASE,
+                status=OperationTestStatus.IN_PROGRESS,
+            )
+        )
+
+
+    @allure.step("Produce completed purchase operation event")
+    def produce_completed_purchase_operation_event(self) -> OperationEventTestSchema:
+        return self.produce_operation_event_api(
+            OperationEventTestSchema(
+                type=OperationTestType.PURCHASE,
+                status=OperationTestStatus.COMPLETED,
+            )
+        )
+
+
+def build_operations_kafka_producer_test_client() -> OperationsKafkaProducerTestClient:
+    return OperationsKafkaProducerTestClient(
+        config=test_settings.operations_kafka_client,
+        logger=get_test_logger("OPERATIONS_KAFKA_PRODUCER_TEST_CLIENT"),
+    )
+
+    
--- tests/clients/kafka/producer.py
+++ tests/clients/kafka/producer.py
@@ -1,2 +1,36 @@
+import logging
+
+from tests.tools import allure
+from tests.tools.config.kafka import KafkaClientTestConfig
+from confluent_kafka import Producer
+
+
 class KafkaProducerTestClient:
-    
+    def __init__(
+            self,
+            config: KafkaClientTestConfig,
+            logger: logging.Logger
+    ):
+        self.logger = logger
+        self.producer = Producer(
+            {
+                "bootstrap.servers": config.bootstrap_servers,
+            }
+        )
+
+
+    @allure.step("Produce message to topic {topic}")
+    def produce(self, topic: str, value: str | bytes):
+        try:
+            self.producer.produce(topic, value)
+            self.producer.poll(0)
+            self.logger.info(f"Kafka message produced {topic}")
+        except Exception as error:
+            self.logger.exception(f"Kafka produce failed ${topic}:{error}")
+
+
+    @allure.step('Flush all mesages')
+    def flush_all(self, timeout: float = 10.0):
+        self.logger.info("Kafka producer flush stated")
+        self.producer.flush(timeout)
+        self.logger.info("Kafka producer flush finished")
